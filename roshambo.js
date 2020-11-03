dff5437b31e1c653e3e3136f9293b7baa488eb77
GITerG01N <ladaneferguson@protonmail.com>
2020-11-03 17:14:11 -0500

    Set functions for each button

--- roshambo.js
+++ roshambo.js
@@ -9,7 +9,6 @@ let losses = 0;
 let draws = 0;
 
 function playRound(playerSelection, computerSelection) {
-	// your code here!
     let Player = playerSelection.toLowerCase();
     let CPU = computerSelection;
     let results = '';
@@ -26,10 +25,10 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
-function game() {
+function game1() {
     let games = 0; 
-    while (games < 5) {
-        let playerSelection = prompt('CHOOSE YOUR FIGHTER!!!1!!111');
+    while (games < 1) {
+        let playerSelection = 'rock';
         const computerSelection = computerPlay();
         console.log(playRound(playerSelection, computerSelection));
         games++;
@@ -42,4 +41,44 @@ function game() {
             console.log(`Whoa... It's a tie!! :O Player - ${wins} | CPU ${losses} | Draws - ${draws}`);
         }
 }
-        
+function game2() {
+    let games = 0; 
+    while (games < 1) {
+        let playerSelection = 'paper';
+        const computerSelection = computerPlay();
+        console.log(playRound(playerSelection, computerSelection));
+        games++;
+    }
+    if (wins > losses) {
+            console.log(`CONGRATULATIONS!!!1!!111 >:D Player - ${wins} | CPU - ${losses} | Draws - ${draws}`);
+        } else if (wins < losses) {
+            console.log(`you lost the match... :( Player - ${wins} | CPU - ${losses} | Draws - ${draws}`);
+        } else {
+            console.log(`Whoa... It's a tie!! :O Player - ${wins} | CPU ${losses} | Draws - ${draws}`);
+        }
+}
+function game3() {
+    let games = 0; 
+    while (games < 1) {
+        let playerSelection = 'scissors';
+        const computerSelection = computerPlay();
+        console.log(playRound(playerSelection, computerSelection));
+        games++;
+    }
+    if (wins > losses) {
+            console.log(`CONGRATULATIONS!!!1!!111 >:D Player - ${wins} | CPU - ${losses} | Draws - ${draws}`);
+        } else if (wins < losses) {
+            console.log(`you lost the match... :( Player - ${wins} | CPU - ${losses} | Draws - ${draws}`);
+        } else {
+            console.log(`Whoa... It's a tie!! :O Player - ${wins} | CPU ${losses} | Draws - ${draws}`);
+        }
+}
+
+const rock = document.querySelector('#rock');
+rock.addEventListener('click', game1);
+
+const paper = document.querySelector('#paper');
+paper.addEventListener('click', game2);
+
+const scissors = document.querySelector('#scissors');
+scissors.addEventListener('click', game3);
